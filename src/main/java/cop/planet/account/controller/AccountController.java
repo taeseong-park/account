79550dd683a8a4db316586187a990efc8c8f9dc8
taeseong <pts0583@naver.com>
2021-05-10 20:12:07 +0900

    default

--- src/main/java/cop/planet/account/controller/AccountController.java
+++ src/main/java/cop/planet/account/controller/AccountController.java
@@ -6,11 +6,9 @@ import cop.planet.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
--- src/main/java/cop/planet/account/model/UserVo.java
+++ src/main/java/cop/planet/account/model/UserVo.java
@@ -3,7 +3,6 @@ package cop.planet.account.model;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
